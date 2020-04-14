7a2bab33c57894cc4bf16939ae50526c61df3fcc
との <ontamage@gmail.com>
2020-04-14 22:07:44 +0900

    訂正

--- js/script.js
+++ js/script.js
@@ -1,38 +1,43 @@
-//スティッキーヘッダー
 
-$(function() {
-  var $win = $(window),
-    $fv = $(".fv"),
-    $header = $("header"),
-    fvHeight = $fv.outerHeight();
-  fixedClass = "fixed";
+//sticky header
 
-  $win.on("load scroll", function() {
+  $(function() {
+    var $win = $(window),
+        $fv = $('.fv'),
+        $header = $('.header')
+        fvHeight = $fv.outerHeight(),
+        fixedClass = 'fixed';
+  
+  $win.on('load scroll',function(){
     var value = $(this).scrollTop();
-    if ($win.width() > 768) {
-      if (value > fvHeight) {
-        $header.addClass("fixed");
-      } else {
-        $header.removeClass("fixed");
-      }
-    }
+      if($win.width()>768)
+  
+        if ( value > fvHeight) {
+          $header.addClass(fixedClass);
+          
+        } else {
+          $header.removeClass(fixedClass);
+          
+        }
+      
+  
+    });
   });
-});
 
-//スライダー
 
-$(".slider").slick({
-  autoplay: true,
-  autoplaySpeed: 1000,
-  speed: 1000,
-  fade: true,
-  cssEase: "linear"
-});
+  //ファーストビューのslider
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear'
+  });
 
-//ハンバーガーメニュー
-
-$(".burger-btn").on("click", function() {
-  $(".header-nav").fadeToggle(300);
-  $(this).toggleClass("cross");
-  $("body").toggleClass("noscroll");
-});
+  
+  //ハンバーガーメニューの開閉
+  $('.burger-btn').on('click',function(){
+    $('.header-nav').fadeToggle(300);
+    $('.burger-btn').toggleClass('cross');
+    $('body').toggleClass('noscroll');
+  });
